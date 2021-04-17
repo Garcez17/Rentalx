6b7c9b469a4e59352f29dd83a7f4753147668b10
Garcez17 <ggarcez613@gmail.com>
2021-04-16 21:51:14 -0300

    docker compose database and app

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,10 @@ import SwaggerUi from 'swagger-ui-express';
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
+import './database';
+
 const app = express();
+
 app.use(express.json());
 app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(swaggerFile));
 
